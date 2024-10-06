81e6293a8c8cdfccc989f019412e441126f6bc30
Ludmila Gonzalez <gonzalezludmila36@gmail.com>
2024-10-05 23:16:59 -0300

    ApiDocumento

---
+++ ArgenMoto/Controllers/DocumentoController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Interfaces.Services;
+using Application.Models;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace ArgenMoto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentoController : ControllerBase
+    {
+        private readonly IDocumentoService _service;
+
+        public DocumentoController(IDocumentoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _service.ListaDocumento();
+
+                if (result.Any())
+                {
+                    return new JsonResult(result) { StatusCode = 200 };
+                }
+
+                return new JsonResult(result) { StatusCode = 200 };
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult MedioPagoPorId(int id)
+        {
+            try
+            {
+                var result = _service.DocumentoPorId(id);
+
+                if (result != null)
+                {
+                    return new JsonResult(result) { StatusCode = 200 };
+                }
+
+                return new JsonResult(result) { StatusCode = 200 };
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+    }
+}
